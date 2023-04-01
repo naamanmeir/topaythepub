5266b811abf77563c525d52047d1c90b3aac10dd
Naaman Meir <namnamon@gmail.com>
2023-04-01 20:52:44 +0000

    ze oved shuv now let keep buidlingit

--- public/js/script2.js
+++ public/js/script2.js
@@ -1,6 +1,8 @@
-const timerRefreshMainDivsTime = 995000;
+const timerRefreshMainDivsTime = 9995000;
+const timerRefreshContentTime = 9995000;
 const timerRefreshElementsTime = 9995000;
-const timerRefreshCssTime = 5000;
+const timerRefreshCssTime = 9995000;
+
 
 const divHeader = document.getElementById("divHeader");
 const divTopMenu = document.getElementById("divTopMenu");
@@ -9,15 +11,19 @@ const divFloatMenu = document.getElementById("divFloatMenu");
 const divContent = document.getElementById("divContent");
 const divFooter = document.getElementById("divFooter");
 
+let appendedScriptObjectContent;
+
+let searchBox1;
+
 window.addEventListener('load', loadUtiliti, false);
 function loadUtiliti() {
     setTimeout(populateMainDivs(), 100);
     let timerRefreshMainDivs = setInterval(populateMainDivs, timerRefreshMainDivsTime);
+    let timerRefreshContent = setInterval(populateContent, timerRefreshContentTime);
     setTimeout(populateElements(), 110);
     let timerRefreshElements = setInterval(populateElements, timerRefreshElementsTime);
     let timerRefreshCss = setInterval(refreshCss, timerRefreshCssTime);
 };
-
 
 function populateMainDivs() {
     getRequest("./app/header", displayHeader);
@@ -26,6 +32,10 @@ function populateMainDivs() {
     getRequest("./app/floatMenu", displayFloatMenu);
     getRequest("./app/content", displayContent);
     getRequest("./app/footer", displayFooter);
+};
+
+function populateContent() {
+    getRequest("./app/content", displayContent);
 };
 
 function populateElements() {
@@ -42,19 +52,23 @@ function displaySideMenu(content) {
     divSideMenu.innerHTML = (content);
     divSideMenu.className = "hidden";
 };
-
 function displayFloatMenu(content) {
     divFloatMenu.innerHTML = (content);
 };
+
 function displayContent(content) {
     divContent.innerHTML = (content);
+    appendScriptContent();
+};
+function appendScriptContent() {
     let scriptSrc = document.getElementById("contentScript");
     var script = document.createElement("script");
-    script.src = (scriptSrc.src);
+    script.src = (scriptSrc.src) + '?id=' + randomNumberGen();
     scriptSrc.remove();
     divContent.append(script);
     populateProducts();
 };
+
 function displayFooter(content) {
     divFooter.innerHTML = (content);
 };
@@ -85,7 +99,29 @@ async function getRequest(url, callback, data) {
     };
 
 };
+//------------------------SEND PORT REQUEST TO: url WITH -> callback function AND APPENDED data----------------
+async function postRequest(url, callback, data) {
+    var xhttp = new XMLHttpRequest();
+    console.log("SENDING POST REQUEST TO: " + url);
+    if (data != null) { xhttp.open("POST", url + data, true); }
+    if (data == null) { xhttp.open("POST", url, true); }
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // console.log("RESPONSE: " + this.response);
+            if (callback != null) { callback(this.response); }
+            return this.response;
+        };
+    };
 
+};
+//------------------------FAKE FUNCTION TO NULL RESPONSES----------------
+async function responseToNull(res) {
+    console.log("response went to null");
+    console.log(res);
+    res = null;
+    delete res;
+}
 function refreshCss() {
     let links = document.getElementsByTagName('link');
     for (let i = 0; i < links.length; i++) {
@@ -97,4 +133,9 @@ function refreshCss() {
             links[i].setAttribute('href', newHref);
         }
     }
+};
+
+function randomNumberGen() {
+    var val = Math.floor(1000 + Math.random() * 9000);
+    return val;
 };
--- public/js/script_content.js
+++ public/js/script_content.js
@@ -1,3 +1,4 @@
+divContent.style.backgroundColor = "black";
 // divContent.style.backgroundColor = generateRandomColor();
 
 function generateRandomColor() {
@@ -7,4 +8,83 @@ function generateRandomColor() {
     randomNumber = randomNumber.toString(16);
     let randColor = randomNumber.padStart(6, 0);
     return `#${randColor.toUpperCase()}`
+};
+
+searchBox1 = document.getElementById("searchBox");
+searchBox1.addEventListener('focus', function () {
+    console.log("focus");
+    searchBoxClear();
+    if (searchBox1.value.length > 0) { searchBox1.placeholder = (""); };
+});
+searchBox1.addEventListener('blur', function () {
+    console.log("blur");
+    searchBox1.placeholder = ("שלום הכניסו שם✍👉👉");
+});
+searchBox1.addEventListener('input', function () {
+    console.log("input");
+    let input = searchBox1.value;
+    sendQuery(input);
+});
+
+async function sendQuery(query) {
+    let response = await postRequest('./app/searchName/', window.parent.parseQuery, query);
+};
+
+function parseQuery(data) {
+    data = JSON.parse(data);
+    console.log(data);
+    autoComplete(data);
+};
+
+function searchBoxClear() {
+    clearAutoComplete(document.getElementById("autoComplete"));
+    const searchBox = document.getElementById("searchBox");
+    if (searchBox1.value.length == 0) { userSearchMessage(0); };
+    if (searchBox.value == "") { userSearchMessage(0); };
+};
+
+function foundNames(query, clients, dest) {
+    // console.log(clients);
+    names = [];
+    for (i = 0; i < clients.length; i++) {
+        names.push(clients[i].nick);
+    };
+    autoComplete(names);
+};
+
+function autoComplete(names) {
+    const autoDiv = document.getElementById("autoComplete");
+    clearAutoComplete(autoDiv);
+    autoDiv.className = "autoCompleteSuggestions";
+    for (i = 0; i < names.length && i < 4; i++) {
+        const para = document.createElement("p");
+        para.className = "autocomplete-items";
+        if (i % 2 === 0) { para.classList.add("autocomplete-itemsEven"); }
+        para.innerText = names[i].nick;
+        autoDiv.appendChild(para);
+        para.onclick = function () {
+            copyTextToSearchBox(para.innerText);
+            loginFunction(para.innerText);
+            clearAutoComplete(autoDiv);
+        }
+    };
+    if (names[0] == searchBox1.value) {
+        clearAutoComplete(autoDiv);
+        userSearchMessage(3);
+        searchBox1.blur();
+    };
+    if (searchBox1.value.length == 0) { userSearchMessage(0); };
+    if (searchBox1.value == "") { clearAutoComplete(autoDiv); }
+};
+
+function clearAutoComplete(autoDiv) {
+    autoDiv.className = "autoCompleteNone";
+    while (autoDiv.hasChildNodes()) {
+        autoDiv.removeChild(autoDiv.firstChild);
+    };
+};
+
+function copyTextToSearchBox(text) {
+    const searchBox = document.getElementById("searchBox");
+    searchBox.value = text;
 };
